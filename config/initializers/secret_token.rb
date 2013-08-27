d26a3bc06db383bb67f8ea36c41d708bf7af3091
Ivan Ardila <icasardila@gmail.com>
2013-08-27 11:51:58 -0500

    Intsalling Rails(4.0) App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KoombeaLibrary::Application.config.secret_key_base = '24ec91cbd2a3391988278f2dffab8298a59220e8868c6c3c9abcfa3bfdf4ad1c91c1fc35fb0771a19c77ab280e7fa7c83ee1483f12fd5cf8a7e483368da79d78'
