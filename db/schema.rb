00695ccde0337439a54128029f9879cde6007442
Ivan Ardila <icas@Ivans-MacBook-Pro.local>
2014-11-10 15:01:10 -0500

    Add isbn number to book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110194452) do
+ActiveRecord::Schema.define(version: 20141110200017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20141110194452) do
     t.string   "format"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "isbn"
   end
 
   create_table "editorials", force: true do |t|
