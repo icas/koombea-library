0e4c8a4789a51bda9bf296f348849360d9ac1fb6
Ivan Ardila <icas@Ivans-MacBook-Pro.local>
2014-11-10 14:47:27 -0500

    Add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 KoombeaLibrary::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :pages, only: [:index]
   match 'auth/:provider/callback', to: 'sessions#create', via: :post
   get "logout", to: "sessions#destroy", as: "logout"
